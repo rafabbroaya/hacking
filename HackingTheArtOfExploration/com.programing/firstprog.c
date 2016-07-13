72c663e75c11644fc76ad7b8d82354948c4db8c8
rsanchez (job machine) <rafabbroaya@hotmail.com>
2016-07-13 16:47:30 -0500

    INIT

---
+++ HackingTheArtOfExploration/com.programing/firstprog.c
@@ -0,0 +1,16 @@
+/*
+ * firstprog.c
+ *
+ *  Created on: 13 jul. 2016
+ *      Author: rsanchez
+ */
+#include <stdio.h>
+
+int main() {
+	int i;
+	for (i = 0; i < 10; i++) {
+		puts("Hello, world!");
+	}
+	return 0;
+}
+
